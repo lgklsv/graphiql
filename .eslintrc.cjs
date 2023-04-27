072022c2ef4d79d4a93d835f86e802875e291f0d
lgklsv <kolosov20011@gmail.com>
2023-04-27 21:16:06 +0300

    init: setup vitest config and test coverage

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,5 +24,7 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
+
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    css: true,
+    coverage: {
+      include: ['src/**/*'],
+      exclude: [
+        '**/*/@(index|config).@(tsx|ts)',
+        '**/*/*.@(icon|asset|d|test).@(tsx|ts)',
+      ],
+      enabled: true,
+      provider: 'c8',
+      reporter: ['text'],
+      all: true,
+    },
+  },
+});
