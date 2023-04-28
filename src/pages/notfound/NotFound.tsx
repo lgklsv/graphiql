06a745064e74ddc6af8add37405a8972eaa3885a
Alesia <alesia.litvinenko@gmail.com>
2023-04-28 22:15:35 +0300

    refactor: change style file (replace module)

--- src/pages/notfound/NotFound.tsx
+++ src/pages/notfound/NotFound.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import styles from './NotFound.module.scss';
+import './NotFound.scss';
 
 const NotFound: React.FC = () => {
   return (
@@ -9,14 +9,14 @@ const NotFound: React.FC = () => {
         <title>404</title>
       </Helmet>
 
-      <div className={styles.layout}>
-        <div className={styles.container}>
-          <div className={styles.wrapper_404}>
+      <div className="layout">
+        <div className="container">
+          <div className="wrapper_404">
             <h3>Houston, we have a problem…</h3>
             <h1>
               4
-              <span className={styles.zero_container}>
-                <div className={styles.zero_wrapper}>
+              <span className="zero_container">
+                <div className="zero_wrapper">
                   <img
                     src="https://d33wubrfki0l68.cloudfront.net/86ebb532679ef65ed71e31ddcc1309552bb67321/44b2e/static/astronaut.e366bb92.svg"
                     alt="cosmos"
@@ -26,7 +26,7 @@ const NotFound: React.FC = () => {
               4
             </h1>
           </div>
-          <span className={styles.ground} />
+          <span className="ground" />
         </div>
       </div>
     </>
