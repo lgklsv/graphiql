0ff84a315bf461b7737fe2f6f7a8e4fb9ed9795a
Alesia <alesia.litvinenko@gmail.com>
2023-04-28 16:22:35 +0300

    feat: add not found component

--- src/pages/notfound/NotFound.tsx
+++ src/pages/notfound/NotFound.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Typography } from 'antd';
-
-const { Title } = Typography;
+import styles from './NotFound.module.scss';
 
 const NotFound: React.FC = () => {
   return (
@@ -10,7 +8,27 @@ const NotFound: React.FC = () => {
       <Helmet>
         <title>404</title>
       </Helmet>
-      <Title>404 page goes here</Title>
+
+      <div className={styles.layout}>
+        <div className={styles.container}>
+          <div className={styles.wrapper_404}>
+            <h3>Houston, we have a problem…</h3>
+            <h1>
+              4
+              <span className={styles.zero_container}>
+                <div className={styles.zero_wrapper}>
+                  <img
+                    src="https://d33wubrfki0l68.cloudfront.net/86ebb532679ef65ed71e31ddcc1309552bb67321/44b2e/static/astronaut.e366bb92.svg"
+                    alt="cosmos"
+                  />
+                </div>
+              </span>
+              4
+            </h1>
+          </div>
+          <span className={styles.ground} />
+        </div>
+      </div>
     </>
   );
 };
