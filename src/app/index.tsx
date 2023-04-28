0f954fffeb53c2215faad61ebe57c636e90d94c8
lgklsv <kolosov20011@gmail.com>
2023-04-28 23:47:55 +0300

    fix: test pre-commit again

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { ConfigProvider } from 'antd';
 
-import { appTitle, customTheme } from './config';
 import Routing from 'pages';
+import { appTitle, customTheme } from './config';
 import './index.scss';
 
 const App: React.FC = () => {
