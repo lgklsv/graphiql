3bfd1ba87aa4bf728afb556c7719d7755a74a8f3
marerma <maria.kadantseva@gmail.com>
2023-05-01 17:43:17 +0300

    feat: add developers const

---
+++ src/shared/lib/constants.ts
@@ -0,0 +1,24 @@
+import { IDevCard } from './types';
+
+const DEVELOPERS: IDevCard[] = [
+  {
+    name: 'Alesia Abaeva',
+    github: 'alesia-abaeva',
+    link: 'https://github.com/alesia-abaeva',
+    img: 'https://avatars.githubusercontent.com/u/101274979?v=4',
+  },
+  {
+    name: 'Oleg Kolosov',
+    github: 'lgklsv',
+    link: 'https://github.com/lgklsv',
+    img: 'https://avatars.githubusercontent.com/u/101424508?v=4',
+  },
+  {
+    name: 'Maria Ermolaeva',
+    github: 'marerma',
+    link: 'https://github.com/marerma',
+    img: 'https://avatars.githubusercontent.com/u/106168393?v=4',
+  },
+];
+
+export { DEVELOPERS };
