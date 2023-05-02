588a961cf74c20c732fb473bf893aad59809a31f
marerma <maria.kadantseva@gmail.com>
2023-05-02 14:58:44 +0300

    feat: add basic i18n config

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import './shared/lib/i18n/i18n';
 
 import App from 'app';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Suspense fallback={<p>is Loading...</p>}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Suspense>
   </React.StrictMode>
 );
