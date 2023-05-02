296e81b5a795e7a509208ac86231ac23c221d307
lgklsv <kolosov20011@gmail.com>
2023-05-02 17:18:31 +0300

    feat: implement sidebar buttons with tooltips

--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Button, Space, Tooltip } from 'antd';
+import {
+  BookOutlined,
+  MacCommandOutlined,
+  ReloadOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
 
 import styles from './Sidebar.module.scss';
 
 const Sidebar: React.FC = () => {
-  return <div className={styles.sidebar} />;
+  return (
+    <div className={styles.sidebar}>
+      <Tooltip placement="bottomLeft" title="Show documentation">
+        <Button type="text" size="large" icon={<BookOutlined />} />
+      </Tooltip>
+      <Space direction="vertical">
+        <Tooltip placement="bottomLeft" title="Re-fetch GraphQL schema">
+          <Button type="text" size="large" icon={<ReloadOutlined />} />
+        </Tooltip>
+        <Tooltip placement="bottomLeft" title="Explore all shortcuts">
+          <Button type="text" size="large" icon={<MacCommandOutlined />} />
+        </Tooltip>
+        <Tooltip placement="bottomLeft" title="Open settings">
+          <Button type="text" size="large" icon={<SettingOutlined />} />
+        </Tooltip>
+      </Space>
+    </div>
+  );
 };
 
 export default Sidebar;
