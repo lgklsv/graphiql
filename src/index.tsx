1c7db1a481a674d6fa61b0e2cb664ce2d66234a3
marerma <maria.kadantseva@gmail.com>
2023-05-03 16:57:58 +0300

    fix: replace fallback in suspence while translation loading

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import './shared/lib/i18n/i18n';
 
 import App from 'app';
+import Spinner from 'shared/ui/Spinner/Spinner';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Suspense fallback={<p>is Loading...</p>}>
+    <Suspense fallback={<Spinner size="large" />}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
