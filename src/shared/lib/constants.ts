821df17034c9ce6e90404a071b1cefa9e18a0e05
marerma <maria.kadantseva@gmail.com>
2023-05-03 15:35:36 +0300

    fix: adjust dev data and type for translation

--- src/shared/lib/constants.ts
+++ src/shared/lib/constants.ts
@@ -4,18 +4,21 @@ const DEVELOPERS: IDevCard[] = [
     github: 'alesia-abaeva',
     link: 'https://github.com/alesia-abaeva',
     img: 'https://avatars.githubusercontent.com/u/101274979?v=4',
+    translationKey: 'alesiaAbaeva',
   },
   {
     name: 'Oleg Kolosov',
     github: 'lgklsv',
     link: 'https://github.com/lgklsv',
     img: 'https://avatars.githubusercontent.com/u/101424508?v=4',
+    translationKey: 'olegKolosov',
   },
   {
     name: 'Maria Ermolaeva',
     github: 'marerma',
     link: 'https://github.com/marerma',
     img: 'https://avatars.githubusercontent.com/u/106168393?v=4',
+    translationKey: 'mariaErmolaeva',
   },
 ];
 
