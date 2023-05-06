48d64dfc078637e63176dab839bff2a480d69830
lgklsv <kolosov20011@gmail.com>
2023-05-06 23:16:50 +0300

    feat: polish code autocomplete field

--- src/features/query/editor/config.ts
+++ src/features/query/editor/config.ts
@@ -28,6 +28,8 @@ const colorBgSecondary = '#f1f2f4';
 const APP_THEME = EditorView.theme({
   '&': {
     color: colorPrimary,
+    fontSize: '16px',
+    fontWeight: 500,
   },
   '.cm-content': {
     caretColor: colorPrimary,
@@ -38,22 +40,44 @@ const APP_THEME = EditorView.theme({
   '.cm-gutters': {
     backgroundColor: 'transparent',
     border: 'none',
+    userSelect: 'none',
+  },
+  '.cm-lineNumbers': {
+    minWidth: '1.8rem',
   },
   '.cm-selectionLayer .cm-selectionBackground': {
     background: 'transparent',
   },
   '.cm-tooltip': {
-    backgroundColor: colorBgSecondary,
+    backgroundColor: '#fff',
     color: colorPrimary,
     border: 'none',
-    padding: '2px 7px',
-    borderRadius: '4px',
+    padding: '5px',
+    borderRadius: '6px',
+    boxShadow:
+      '0px 6px 20px rgba(59, 76, 106, 0.13), 0px 1.34018px 4.46726px rgba(59, 76, 106, 0.0774939), 0px 0.399006px 1.33002px rgba(59, 76, 106, 0.0525061)',
   },
   '.cm-tooltip-hover': {
     backgroundColor: colorBgSecondary,
   },
   '.cm-tooltip.cm-tooltip-cursor': {
     backgroundColor: colorBgBase,
+  },
+  '.cm-tooltip-autocomplete ul li[role="option"]': {
+    borderRadius: '4px',
+    backgroundColor: '#fff',
+    color: colorPrimary,
+    padding: '5px',
+  },
+  '.cm-tooltip-autocomplete ul li[aria-selected]': {
+    backgroundColor: `hsla(293, 100%, 27%, 0.1);`,
+    color: '#770088',
+    padding: '5px',
+  },
+  '.cm-tooltip-autocomplete ul': {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '0.2rem',
   },
 });
 
