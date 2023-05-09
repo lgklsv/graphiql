46c9c58276dd208faefaab72769f7b438ac091b8
marerma <maria.kadantseva@gmail.com>
2023-05-09 16:37:51 +0300

    feat: add persist config to app and reset state at logout

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,23 +1,52 @@
 import {
+  AnyAction,
   combineReducers,
   configureStore,
   PreloadedState,
 } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import { graphql } from 'shared/api';
-import userReducer from './reducers/UserSlice';
+import userReducer, { removeUser } from './reducers/UserSlice';
 import docsReducer from './reducers/DocsSlice';
 import tabsReducer from './reducers/TabSlice';
 
-const rootReducer = combineReducers({
+const persistConfig = {
+  key: 'graphiql',
+  storage,
+  throttle: 400,
+  blacklist: [graphql.sandboxQueries.reducerPath], // we exclude api-slices from saving in localStorage
+};
+
+const appReducer = combineReducers({
   userReducer,
   docsReducer,
   tabsReducer,
   [graphql.sandboxQueries.reducerPath]: graphql.sandboxQueries.reducer,
 });
 
+// When user logs out, the state is reset to the initial "fresh" state.
+// If the action 'user/removeUser' is fired (clicking logout-button and deleting user data), we return undefined as state.
+// That means for redux - load the fresh initial state/reset state.
+// In this case we can reset state without reloading the page.
+// It assures that if the user logs in on the welcome page with other login and without reloading, he/she will get a fresh sandbox.
+
+const rootReducer = (
+  state: ReturnType<typeof appReducer> | undefined,
+  action: AnyAction
+) => {
+  if (action.type === removeUser) {
+    storage.removeItem('persist:graphiql');
+    return appReducer(undefined, action);
+  }
+  return appReducer(state, action);
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
   return configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     preloadedState,
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
@@ -25,6 +54,9 @@ export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
       }).concat(graphql.sandboxQueries.middleware),
   });
 };
+
+export const store = setupStore();
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppState = ReturnType<typeof setupStore>;
