dfb2cf592bc0e424df9426015784ae77f0da1953
lgklsv <kolosov20011@gmail.com>
2023-05-12 23:25:16 +0300

    feat: add better colors levels to timing indicator

--- src/entities/response/lib/index.ts
+++ src/entities/response/lib/index.ts
@@ -1,7 +1,16 @@
 export const getTimingColor = (timing: number): string => {
-  let timingColor = 'default';
-  if (timing) {
-    timingColor = timing > 200 ? 'error' : 'success';
+  let timingColor;
+  switch (true) {
+    case timing <= 200:
+      timingColor = 'success';
+      break;
+    case timing > 1000:
+      timingColor = 'error';
+      break;
+    default:
+      timingColor = 'default';
+      break;
   }
+
   return timingColor;
 };
