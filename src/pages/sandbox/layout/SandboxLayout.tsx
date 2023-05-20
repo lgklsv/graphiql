20f04e58ecad7060534de17510eeab820eaac468
Alesia <alesia.litvinenko@gmail.com>
2023-05-20 18:17:20 +0300

    refactor: moved firestore initial loadung

--- src/pages/sandbox/layout/SandboxLayout.tsx
+++ src/pages/sandbox/layout/SandboxLayout.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-
 import { SessionTabs } from 'features/tabs';
 import { Sidebar } from 'widgets/sidebar';
 import { ApiConnector } from 'features/api-connector';
 import { QueryField } from 'entities/query';
 import { ResponseField } from 'entities/response';
 import { ErrorBoundary } from 'shared/hoc';
-import { useSetFirestore } from 'shared/lib/firestore/hook/use-set-firestore';
-import { useAuthState } from 'shared/hooks/use-auth';
 
 import styles from './SandboxLayout.module.scss';
 
 const SandboxLayout: React.FC = () => {
-  const { id } = useAuthState();
-  const firestoreDispatch = useSetFirestore();
-
-  React.useEffect(() => {
-    firestoreDispatch(id as string);
-  }, [firestoreDispatch, id]);
-
-  // TODO: add loader on respons firestore
-
   return (
     <section className={styles.layout}>
       <ErrorBoundary type="notification">
