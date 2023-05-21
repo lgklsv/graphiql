7bb20b8998aa3195205201482f8322febeb1857e
marerma <maria.kadantseva@gmail.com>
2023-05-21 18:07:25 +0300

    feat: add signOut to logout btn

--- src/widgets/header/ui/Nav/Nav.tsx
+++ src/widgets/header/ui/Nav/Nav.tsx
@@ -1,4 +1,4 @@
-import { Button, Space } from 'antd';
+import { Button, Space, message } from 'antd';
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -8,6 +8,7 @@ import { removeUser } from 'store/reducers/UserSlice';
 import { LangSwitcher } from 'features/langSwitcher';
 import { useAuthState } from 'shared/hooks/use-auth';
 import { useAppDispatch } from 'shared/hooks/redux';
+import { auth, signOut } from 'firebase';
 import styles from './Nav.module.scss';
 
 interface NavProps {
@@ -21,10 +22,18 @@ const Nav: React.FC<NavProps> = ({ mobile, toggle }) => {
   const location = useLocation();
   const { isAuth } = useAuthState();
   const dispatch = useAppDispatch();
+  const [messageApi, contextHolder] = message.useMessage();
 
   const handleLogOut = () => {
-    dispatch(removeUser());
-    navigate(ROUTES.home, { replace: true });
+    signOut(auth)
+      .then(() => dispatch(removeUser()))
+      .catch(() => {
+        messageApi.open({
+          type: 'error',
+          content: `${t('errorBoundary.errorSignOut')}`,
+        });
+      })
+      .finally(() => navigate(ROUTES.home, { replace: true }));
   };
 
   const buttonClickHandler = (route: string) => {
@@ -35,54 +44,57 @@ const Nav: React.FC<NavProps> = ({ mobile, toggle }) => {
   };
 
   return (
-    <Space
-      size={15}
-      direction={mobile ? 'vertical' : 'horizontal'}
-      className={mobile ? '' : styles.nav}
-    >
-      {isAuth && location.pathname !== ROUTES.home && (
-        <Button
-          size="large"
-          onClick={() => buttonClickHandler(ROUTES.home)}
-          className={styles.nav__btn}
-        >
-          {t('button.home')}
-        </Button>
-      )}
-      {isAuth && location.pathname === ROUTES.home && (
-        <Button
-          size="large"
-          onClick={() => buttonClickHandler(ROUTES.sandbox)}
-          className={styles.nav__btn}
-        >
-          {t('button.sandbox')}
-        </Button>
-      )}
-      <LangSwitcher />
-      {isAuth ? (
-        <Button type="primary" size="large" onClick={handleLogOut}>
-          {t('button.logout')}
-        </Button>
-      ) : (
-        <>
+    <>
+      {contextHolder}
+      <Space
+        size={15}
+        direction={mobile ? 'vertical' : 'horizontal'}
+        className={mobile ? '' : styles.nav}
+      >
+        {isAuth && location.pathname !== ROUTES.home && (
           <Button
             size="large"
-            onClick={() => buttonClickHandler(ROUTES.signup)}
+            onClick={() => buttonClickHandler(ROUTES.home)}
             className={styles.nav__btn}
           >
-            {t('button.signup')}
+            {t('button.home')}
           </Button>
+        )}
+        {isAuth && location.pathname === ROUTES.home && (
           <Button
-            type="primary"
             size="large"
-            onClick={() => buttonClickHandler(ROUTES.login)}
+            onClick={() => buttonClickHandler(ROUTES.sandbox)}
             className={styles.nav__btn}
           >
-            {t('button.login')}
+            {t('button.sandbox')}
           </Button>
-        </>
-      )}
-    </Space>
+        )}
+        <LangSwitcher />
+        {isAuth ? (
+          <Button type="primary" size="large" onClick={handleLogOut}>
+            {t('button.logout')}
+          </Button>
+        ) : (
+          <>
+            <Button
+              size="large"
+              onClick={() => buttonClickHandler(ROUTES.signup)}
+              className={styles.nav__btn}
+            >
+              {t('button.signup')}
+            </Button>
+            <Button
+              type="primary"
+              size="large"
+              onClick={() => buttonClickHandler(ROUTES.login)}
+              className={styles.nav__btn}
+            >
+              {t('button.login')}
+            </Button>
+          </>
+        )}
+      </Space>
+    </>
   );
 };
 
