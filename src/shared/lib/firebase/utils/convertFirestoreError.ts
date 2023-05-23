23c99db2d770dd9c30a68cf686b83607d9af5549
lgklsv <kolosov20011@gmail.com>
2023-05-23 19:33:02 +0300

    feat: add more custom errors for firebase auth

--- src/shared/lib/firebase/utils/convertFirestoreError.ts
+++ src/shared/lib/firebase/utils/convertFirestoreError.ts
@@ -12,6 +12,12 @@ export const convertFirestoreError = (message: string): string => {
     case message.includes('email-already-in-use'):
       convertedMessage = t('firestoreErrors.accountExists');
       break;
+    case message.includes('wrong-password'):
+      convertedMessage = t('firestoreErrors.wrongPassword');
+      break;
+    case message.includes('user-not-found'):
+      convertedMessage = t('firestoreErrors.noUser');
+      break;
     default:
       convertedMessage = message;
       break;
